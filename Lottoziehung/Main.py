2ff98c425d864cee84124cb4dd3ebf21614dfdf6
Jpaati <p.jenewein@icloud.com>
2022-09-28 08:30:15 +0200

    added Germany

--- Lottoziehung/Main.py
+++ Lottoziehung/Main.py
@@ -1,18 +1,21 @@
 from random import randint
 
-def get_random_six():
+endValueZ = 49
+startValueZ = 0
+rangeZ = 7
+
+def get_random_six(startValue, endValue, range):
     list_return = []
     counter = 0
-    while counter < 6:
-        ran = randint(0,45)
+    while counter < range:
+        ran = randint(startValue, endValue)             #range immer -1 hinten
         if(not ran in list_return):
             list_return.append(ran)
             counter = counter +1
     print(list_return)
     return list_return
     
-
-list_stat = [0] * 46
+list_stat = [0] * (endValueZ + 1)
 
 def get_statistic(list):
     for element in list:
@@ -27,7 +30,7 @@ def print_list(list):
 
 
 for index in range(0, 1000):
-    rand = get_random_six()
+    rand = get_random_six(startValueZ, endValueZ, rangeZ)
     get_statistic(rand)
 
 print_list(list_stat)
