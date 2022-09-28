bf8f8477b2ad06d9399c6aba45aedfb1b4863dcd
Jpaati <p.jenewein@icloud.com>
2022-09-28 09:39:19 +0200

    changes

--- Lottoziehung/Main.py
+++ Lottoziehung/Main.py
@@ -1,25 +1,25 @@
 from random import randint
 
-endValueZ = 49
-startValueZ = 0
-rangeZ = 7
-
-def get_random_six(startValue, endValue, range):
+def get_random_six(startValue, endValue, rangeD):
     list_return = []
-    counter = 0
-    while counter < range:
-        ran = randint(startValue, endValue)             #range immer -1 hinten
-        if(not ran in list_return):
-            list_return.append(ran)
-            counter = counter +1
-    print(list_return)
-    return list_return
+    for item in range(startValue, endValue+1):
+        list_return.append(item)
+        list_stat[item] = 0
+    print(list_return) 
+    print(list_stat)   
     
-list_stat = [0] * (endValueZ + 1)
+    for el in range(rangeD):
+        lastPos = len(list_return)-1-el
+        print(lastPos)
+        ran = randint(startValue, endValue)
+        print(ran)            #range immer -1 hinten
+        list_return[ran], list_return[lastPos] = list_return[lastPos], list_return[ran] 
+    print(list_return)
+    return list_return[:-rangeD]
 
 def get_statistic(list):
     for element in list:
-        list_stat[element] = list_stat[element] +1
+        list_stat[element+startValueZ] = list_stat[element+startValueZ] +1
 
 
 def print_list(list):
@@ -28,11 +28,18 @@ def print_list(list):
         print("Zahl" , counter, ":",element)
         counter = counter +1
 
+endValueZ = 95
+startValueZ = 50
+rangeZ = 6
 
-for index in range(0, 1000):
+list_stat = {}
+
+for index in range(0, 1001):
     rand = get_random_six(startValueZ, endValueZ, rangeZ)
+    print(rand)
     get_statistic(rand)
 
-print_list(list_stat)
+
+
 
 
