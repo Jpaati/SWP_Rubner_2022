20b590c1184ee1151e36ea7398f61ee410b17f80
Jpaati <p.jenewein@icloud.com>
2022-10-11 14:31:50 +0200

    dict funktioniert nicht

--- Lottoziehung/Main.py
+++ Lottoziehung/Main.py
@@ -8,22 +8,17 @@ def get_random_six(startValue, endValue, rangeD):
 
     for x in range(rangeD):
         lastPos = len(list_return)-1-x
-        ran = randint(startValue, endValue-1)
+        ran = randint(startValue, lastPos)
         list_return[ran], list_return[lastPos] = list_return[lastPos], list_return[ran] 
-    print("Ziehung", list_return[:-rangeD])
+    print("Ziehung", list_return[-rangeD:])
     #TODO: correct ERROR
-    return list_return[:-rangeD]
+    return list_return[-rangeD:]
 
 def get_statistic(list):
+    global list_stat
     for element in list:
-        list_stat[element+startValueZ-1] = list_stat[element+startValueZ-1] +1
+        list_stat[element+startValueZ-1] += 1 
 
-
-def print_list(list):
-    counter = 0
-    for element in list:
-        print("Zahl" , counter, ":",element)
-        counter = counter +1
 
 endValueZ = 45
 startValueZ = 1
@@ -35,7 +30,7 @@ for index in range(0, 1001):
     rand = get_random_six(startValueZ, endValueZ, rangeZ)
     get_statistic(rand)
     #Statistik not right
-print_list(list_stat)
+print(list_stat)
 
 
 
