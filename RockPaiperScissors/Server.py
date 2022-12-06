c3294ff8e25f86bb97b504dae5473f29733fd575
Jpaati <p.jenewein@icloud.com>
2022-12-06 18:04:16 +0100

    Server, Main_OO

--- RockPaiperScissors/Server.py
+++ RockPaiperScissors/Server.py
@@ -15,7 +15,7 @@ api = Api(app) #Die Flask API
 Base = declarative_base()  # Basisklasse aller in SQLAlchemy verwendeten Klassen
 metadata = Base.metadata
 
-engine = create_engine(r'sqlite:///C:\Users\pjene\Desktop\Schule\Anichstraße-HTL\5.AHWII\Cloud-Computing\File-Server-11\imageServer.sqlite3') #Welche Datenbank wird verwendet
+engine = create_engine(r'sqlite:///C:\Users\pjene\Desktop\Schule\Anichstraße-HTL\5.AHWII\SWP\RockPaperScissor\server.sqlite3') #Welche Datenbank wird verwendet
 db_session = scoped_session(sessionmaker(autocommit=True, autoflush=True, bind=engine))
 Base.query = db_session.query_property() #Dadurch hat jedes Base - Objekt (also auch ein GeoInfo) ein Attribut query für Abfragen
 app = Flask(__name__) #Die Flask-Anwendung
@@ -23,24 +23,16 @@ api = Api(app) #Die Flask API
 
 @dataclass #Diese ermoeglicht das Schreiben als JSON mit jsonify
 class BinaryWithMetadata(Base):
-    __tablename__ = 'images'  # Abbildung auf diese Tabelle
+    __tablename__ = 'data'  # Abbildung auf diese Tabelle
     id: int
-    name: str
-    ext: str
-    data: str
-    desc : str
-    when: str
-    descT : str
-    descAna : str
+    player_name: str
+    symbol: str
+    symbol_num: str
 
     id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    ext = Column(Text)
-    data = Column(Text)
-    desc = Column(Text)
-    when = Column(DateTime, default=func.now())
-    descT = Column(Text)
-    descAna = Column(Text)
+    player_name = Column(Text)
+    symbol = Column(Text)
+    symbol_num = Column(Text)
 
 
 class BinaryWithMetadataREST(Resource):
@@ -50,10 +42,8 @@ class BinaryWithMetadataREST(Resource):
 
     def put(self,id):
         d = request.get_json(force=True)
-        s = translate(d['desc'])
-        ds = analyzeIMG(d['data'])
-        print(ds)
-        info = BinaryWithMetadata(name=d['name'], ext=d['ext'], data=d['data'], desc=d['desc'], descT=s, descAna = ds)
+        print(d)
+        info = BinaryWithMetadata(player_name = d['player_name'], symbol = d['symbol'], symbol_num = d['symbol_num'])
         db_session.add(info)
         db_session.flush()
         print('Addes Session')
@@ -66,32 +56,7 @@ class BinaryWithMetadataREST(Resource):
         db_session.delete(info)
         db_session.flush()
         return jsonify({'message': '%d deleted' % id})
-api.add_resource(BinaryWithMetadataREST, '/img_meta/<int:id>')
-
-
-def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
-
-# Wenn Logging-Information fuer den Client vorhanden ist (gut fuer das Fehlersuchen)
-def on_log(client, userdata, level, buf):
-    print("log: ",buf)
-
-def on_connect(client, userdata, flags, rc, properties=None):
-    print("CONNACK received with code %s." % rc)
-
-@app.route("/getLastImage")
-def get_last_image():
-    res = BinaryWithMetadata.query.all()
-    #todo get last image
-
-@app.route("/search/<string:data>")
-def search(data):
-    res = BinaryWithMetadata.query.filter(or_(BinaryWithMetadata.name.contains(data),BinaryWithMetadata.desc.contains(data))).all()
-    print(res)
-    return jsonify(res)
+api.add_resource(BinaryWithMetadataREST, '/data/<int:id>')
  
 @app.teardown_appcontext
 def shutdown_session(exception=None):
@@ -102,16 +67,8 @@ def init_db():
     # Erzeugen der Tabellen für die Klassen, die oben deklariert sind (muss nicht sein, wenn diese schon existiert)
     Base.metadata.create_all(bind=engine)
 
-def startPicture():
-    client = mqtt.Client('Server')  # Der Parameter ist die client-ID, diese sollte möglichst eindeutig sein.
-    client.username_pw_set('patrick', 'patrick')
-    client.connect('192.168.10.100', port=2222)   # Im Moment verwenden wir die lokale mosquitto Installation, spaeter durch die IP zu ersetzen
-    client.publish("take/picture","TAKE")
-    print("Publishing")
-
 if __name__ == '__main__':
     init_db()
-    #startPicture()
     app.run()
     #app.run(host='192.168.10.100', port=81)
     
---
+++ Ueberpruefung/Main.py
@@ -0,0 +1,134 @@
+
+from enum import Enum
+# Firma hat einen Namen, anzahl Mitarbeiter, anzahl Gruppenleiter, 
+
+class Gender(Enum):
+    Male = 0
+    Female =  1
+
+
+class Firma:
+    def __init__(self, name, ustd_number, abteilungen):   #true für male und false für female
+        self.name = name
+        self.ustd_number = ustd_number
+        self.abteilungen = abteilungen
+    
+    def get_anzahl_Mitarbeiter(self):
+        anz = 0
+        for ab in self.abteilungen:
+            anz += ab.get_anz_mitarbeiter()
+        return anz
+    
+    def get_abteilungsleiter(self):
+        abt = []
+        for ele in self.abteilungen:
+            abt.append(ele.abteilungsleiter)
+        return abt
+    
+    def quote_Female_Male(self):
+        dict_quote = {"Male" : 0, "Female" : 0, "Quote_Woman": 0}
+        all_mit = []
+        for ele in self.abteilungen:
+            all_mit = all_mit + ele.mitarbeiter
+            all_mit.append(ele.abteilungsleiter)
+        for i in all_mit:
+            if(i.geschlecht == Gender.Female):
+                dict_quote["Female"] +=1
+            else:
+                dict_quote["Male"] += 1
+        dict_quote["Quote_Woman"] = dict_quote["Female"]/self.get_anzahl_Mitarbeiter() 
+        return dict_quote
+        
+
+    def get_biggest_department(self):
+        biggest = 0
+        name = ""
+        for ab in self.abteilungen:
+            if(ab.get_anz_mitarbeiter() > biggest):
+                biggest = ab.get_anz_mitarbeiter()
+                name = ab.name
+        return name + "_" + str(biggest)
+
+    def print(self):
+        return self.name + "--" + str(self.ustd_number) + "--" + self.abteilungen
+
+class Abteilung:
+    def __init__(self, name, mitarbeiter, abteilungsleiter):
+        self.name = name
+        self.mitarbeiter = mitarbeiter
+        self.abteilungsleiter = abteilungsleiter
+    
+    def print(self):
+        return "Name: " + self.name +  " Abteilungsleiter: " + self.abteilungsleiter.print()
+
+    def get_anz_mitarbeiter(self):
+        return len(self.mitarbeiter) + 1 #only one Abteilungsleiter
+
+class Person:
+    def __init__(self, name, alter, geschlecht):
+        self.name = name
+        self.alter = alter
+        self.geschlecht = geschlecht
+    
+    def print(self):
+        return "Name: " + self.name + " Alter: " + str(self.alter) + " Geschlecht: " + str(self.geschlecht)
+    
+
+class Mitarbeiter (Person):
+    def __init__(self, name, alter, geschlecht, gehalt):
+        super().__init__(name, alter, geschlecht)
+        self.gehalt = gehalt
+    
+    def print(self):
+        return super().print() + " Gehalt: " + str(self.gehalt)
+    
+
+class Abteilungsleiter (Mitarbeiter):
+    def __init__(self, name, alter, geschlecht, gehalt, parkplatz_number):
+        super().__init__(name, alter, geschlecht, gehalt)
+        self.parkplatz_number = parkplatz_number
+    
+    def print(self):
+        return super().print() + " Parkplatz-Number: " + str(self.parkplatz_number)
+
+
+
+if __name__ == "__main__":
+    mitarbeiter_a = [
+        Mitarbeiter("Patrick", 18, 2005, Gender.Male),
+        Mitarbeiter("Marcel", 18, 2005, Gender.Male),
+        
+    ]
+    mitarbeiter_b = [
+        Mitarbeiter("Paula", 18, 2005, Gender.Male),
+        Mitarbeiter("Timo", 18, 2005, Gender.Male),
+        Mitarbeiter("Noah", 18, 2005, Gender.Male)
+    ]
+
+    a1 = Abteilungsleiter("Hans", 50, Gender.Male, 2005, 20)
+    a2 = Abteilungsleiter("Franzi", 50, Gender.Female, 2005, 20)
+
+    a = Abteilung("Produktion", mitarbeiter_a, a1)
+    b = Abteilung("Qualitätsmanagement", mitarbeiter_b, a2)
+
+    firma = Firma("BMW", 2546, [a, b])
+    
+    #alle Mitarbeiter
+    print("Anzahl Mitarbeiter", firma.get_anzahl_Mitarbeiter())
+    
+    #Die Abteilungsleiter
+    for ele in firma.get_abteilungsleiter():
+        print("Abteilungsleiter", ele.print())
+    
+    #alle Abteilungen
+    for ele in firma.abteilungen:
+        print("Abteilungen", ele.print())
+    
+    #Die größte Abteilung
+    print("Größte Abteilung", firma.get_biggest_department())
+
+    #Quote Frauen, Männer
+    dict_quote = firma.quote_Female_Male()
+    print(dict_quote)
+
+
